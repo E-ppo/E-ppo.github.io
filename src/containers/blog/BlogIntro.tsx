30720e1da1d98f6e3094852f6302507707dcfeab
epppo <amorosso@kakao.com>
2024-12-29 03:31:35 +0900

    [#3] feat : 블로그 에디터 비밀번호 설정

--- src/containers/blog/BlogIntro.tsx
+++ src/containers/blog/BlogIntro.tsx
@@ -1,19 +1,30 @@
 import { ProfileImg } from "@/assets/img"
 import { Card } from "@/components/card"
+import { Modal } from "@/components/modal"
+import PasswordBox from "@/containers/blog/PasswordBox"
 import { theme } from "@/styles/theme"
 import styled from "styled-components"
 
 const BlogIntro = () => {
   return (
-    <Card.Root direction="row" style={{ gap: 20, padding: 16 }}>
-      <Profile src={ProfileImg} alt="" />
-      <Card.Body>
-        <Card.Title>Eppo의 테크 블로그에 오신 것을 환영합니다!</Card.Title>
-        <Card.Description style={{ marginTop: 5, color: theme.colors.dustyRose }}>
-          {`프론트엔드 개발의 다양한 영역을 카테고리별로 정리하고 있습니다. \n개발 과정에서 얻은 인사이트와 실용적인 팁을 공유하며, 함께 성장하는 공간을 만들어가고 있습니다.`}
-        </Card.Description>
-      </Card.Body>
-    </Card.Root>
+    <>
+      <Card.Root direction="row" style={{ gap: 20, padding: 16 }}>
+        <Modal>
+          <Modal.Trigger>
+            <Profile src={ProfileImg} alt="" />
+          </Modal.Trigger>
+          <Modal.Content>
+            <PasswordBox />
+          </Modal.Content>
+        </Modal>
+        <Card.Body>
+          <Card.Title>Eppo의 테크 블로그에 오신 것을 환영합니다!</Card.Title>
+          <Card.Description style={{ marginTop: 5, color: theme.colors.dustyRose }}>
+            {`프론트엔드 개발의 다양한 영역을 카테고리별로 정리하고 있습니다. \n개발 과정에서 얻은 인사이트와 실용적인 팁을 공유하며, 함께 성장하는 공간을 만들어가고 있습니다.`}
+          </Card.Description>
+        </Card.Body>
+      </Card.Root>
+    </>
   )
 }
 
---
+++ src/containers/blog/PasswordBox.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent, useState } from "react"
+import { useNavigate } from "react-router"
+
+const PasswordBox = () => {
+  const [value, setValue] = useState("")
+  const navigation = useNavigate()
+  const password = import.meta.env.VITE_PUBLIC_PASSWORD
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  const handleOnClick = () => {
+    if (value === password) navigation("/blog/editor")
+  }
+
+  return (
+    <div>
+      <input value={value} onChange={handleChange} />
+      <button onClick={handleOnClick}>입력</button>
+    </div>
+  )
+}
+
+export default PasswordBox
