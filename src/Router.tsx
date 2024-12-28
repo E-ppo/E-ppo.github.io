b8d0e9dd189b7a7a51dba13fe2b901dfdfa587ea
epppo <amorosso@kakao.com>
2024-12-28 21:49:15 +0900

    [#2] update :  페이지 라우팅 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
+import { About, Blog, Home, Portfolio } from "@/pages"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "@/pages/Home"
 
 const Router = () => {
   return (
     <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/*<Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import About from "@/pages/About"
+import Blog from "@/pages/Blog"
+import Home from "@/pages/Home"
+import Portfolio from "@/pages/Portfolio"
+
+export { About, Blog, Portfolio, Home }
