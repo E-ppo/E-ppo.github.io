87908471b24a640e4a9ecf3029f6509e1c19f6c4
epppo <amorosso@kakao.com>
2024-12-29 02:09:38 +0900

    deploy : 블로그 중첩 라우팅 및 기본 레이아웃 작성

--- src/components/card/CardBody.tsx
+++ src/components/card/CardBody.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from "react"
+import { HTMLAttributes } from "react"
 import { BaseCardProps } from "@/components/card/type"
 import styled from "styled-components"
 
