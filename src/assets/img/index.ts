0887e2302322299754316523445ab18066424c31
epppo <amorosso@kakao.com>
2024-12-29 02:07:54 +0900

    [#3] update : 카드 컨텐츠 추가

---
+++ src/assets/img/index.ts
@@ -0,0 +1,3 @@
+import ProfileImg from "./eppo.webp"
+
+export { ProfileImg }
--- src/containers/blog/BlogIntro.tsx
+++ src/containers/blog/BlogIntro.tsx
@@ -1,14 +1,26 @@
+import { ProfileImg } from "@/assets/img"
 import { Card } from "@/components/card"
+import { theme } from "@/styles/theme"
+import styled from "styled-components"
 
 const BlogIntro = () => {
   return (
-    <Card.Root direction="row">
+    <Card.Root direction="row" style={{ gap: 20, padding: 16 }}>
+      <Profile src={ProfileImg} alt="" />
       <Card.Body>
-        <Card.Title>제목</Card.Title>
-        <Card.Description>설명</Card.Description>
+        <Card.Title>Eppo의 테크 블로그에 오신 것을 환영합니다!</Card.Title>
+        <Card.Description style={{ marginTop: 5, color: theme.colors.dustyRose }}>
+          {`프론트엔드 개발의 다양한 영역을 카테고리별로 정리하고 있습니다. \n개발 과정에서 얻은 인사이트와 실용적인 팁을 공유하며, 함께 성장하는 공간을 만들어가고 있습니다.`}
+        </Card.Description>
       </Card.Body>
     </Card.Root>
   )
 }
 
 export default BlogIntro
+
+const Profile = styled.img`
+  width: 10vw;
+  min-width: 100px;
+  border-radius: 50%;
+`
